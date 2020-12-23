80a4e174c98f745ee7ee1af3130d6f6f0bfce696
omoluabii <hakra911@gmail.com>
2020-12-23 04:00:51 +0100

    update

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'jquery'
 import 'popper.js'
+import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap'
 import 'bootstrap/dist/css/bootstrap.css';
 import 'boxicons'
